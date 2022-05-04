3a74e47218c6fe8547f9d967d50b9176ec2bd145
Mohit8820 <mohitprasadsah8820@gmail.com>
2022-05-05 00:06:23 +0530

    fixes

--- script.js
+++ script.js
@@ -1,62 +1,62 @@
-// Get the modal
-var log = document.getElementById("log");
-var sign = document.getElementById("sign");
-var logclose = document.getElementById("log-close");
-var signclose = document.getElementById("sign-close");
+// // Get the modal
+// var log = document.getElementById("log");
+// var sign = document.getElementById("sign");
+// var logclose = document.getElementById("log-close");
+// var signclose = document.getElementById("sign-close");
 
-// Get the button that opens the modal
-var signbtn = document.getElementById("signBtn");
-var logbtn = document.getElementById("logBtn");
+// // Get the button that opens the modal
+// var signbtn = document.getElementById("signBtn");
+// var logbtn = document.getElementById("logBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
 
-window.addEventListener("popstate", detectHistory);
+// window.addEventListener("popstate", detectHistory);
 
-function orlog() {
-  sign.style.display = "none";
-  log.style.display = "block";
-}
-function orsign() {
-  sign.style.display = "block";
-  log.style.display = "none";
-}
-// When the user clicks the button, open the modal
-logbtn.onclick = function () {
-  log.style.display = "block";
-  // window.history.pushState({ id: 1 }, null, "q=1234&u=beware");
-};
-signbtn.onclick = function () {
-  sign.style.display = "block";
-  // window.history.pushState({ id: 1 }, null, "q=1234&u=beware");
-};
+// function orlog() {
+//   sign.style.display = "none";
+//   log.style.display = "block";
+// }
+// function orsign() {
+//   sign.style.display = "block";
+//   log.style.display = "none";
+// }
+// // When the user clicks the button, open the modal
+// logbtn.onclick = function () {
+//   log.style.display = "block";
+//   // window.history.pushState({ id: 1 }, null, "q=1234&u=beware");
+// };
+// signbtn.onclick = function () {
+//   sign.style.display = "block";
+//   // window.history.pushState({ id: 1 }, null, "q=1234&u=beware");
+// };
 
-// When the user clicks on <span> (x), close the modal
-logclose.onclick = function () {
-  log.style.display = "none";
-  // history.back();
-};
-signclose.onclick = function () {
-  sign.style.display = "none";
-  // history.back();
-};
+// // When the user clicks on <span> (x), close the modal
+// logclose.onclick = function () {
+//   log.style.display = "none";
+//   // history.back();
+// };
+// signclose.onclick = function () {
+//   sign.style.display = "none";
+//   // history.back();
+// };
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == sign) {
-    sign.style.display = "none";
-    // history.back();
-  }
-  if (event.target == log) {
-    log.style.display = "none";
-    // history.back();
-  }
-};
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function (event) {
+//   if (event.target == sign) {
+//     sign.style.display = "none";
+//     // history.back();
+//   }
+//   if (event.target == log) {
+//     log.style.display = "none";
+//     // history.back();
+//   }
+// };
 
-function detectHistory() {
-  sign.style.display = "none";
-  log.style.display = "none";
-}
+// function detectHistory() {
+//   sign.style.display = "none";
+//   log.style.display = "none";
+// }
 
 // var query = document.getElementsByClassName("query-asked");
 
